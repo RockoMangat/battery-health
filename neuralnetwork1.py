64314b7a1976b5de0a8150fe97742f0d63d03b64
rohanmangat <rohan.mangat@gmail.com>
2023-04-01 13:41:23 +0100

    clean data file

--- neuralnetwork1.py
+++ neuralnetwork1.py
@@ -16,9 +16,31 @@ df3 = frames[2]
 
 allframes = [df1, df2, df3]
 
+# combining all dataframes
 dfcomb = pd.concat(allframes, axis=1)
+print(dfcomb.shape)
 
-print(frames)
+dfcomb = dfcomb.drop(['SOH charge cycles', 'SOH discharge cycles', 'SOH discharge cycles 2', 'SOH discharge 2'], axis = 1)
+print(dfcomb.shape)
+
+# find average SOH for charge and discharge, then remove those 2 columns
+dfcomb['Average SOH'] = dfcomb[['SOH charge', 'SOH discharge']].mean(axis=1)
+dfcomb = dfcomb.drop(['SOH charge', 'SOH discharge'], axis=1)
+
+
+# get the x and y data:
+X = dfcomb.drop('Average SOH', axis=1)
+print(X.shape)
+
+y = dfcomb['Average SOH']
+print(y.shape)
+
+# Split data into x and y, with 30% for testing and randomly shuffling data
+(X_train, X_test, y_train, y_test) = train_test_split(X, y, test_size=0.3, random_state=22)
+
+print('yeee')
+
+# print(frames)
 def nn1(X,Y):
 
     X = np.arange(20).reshape(-1, 1)
