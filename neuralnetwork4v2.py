e925bf8df22eda2db0bb581d8aa115ca873b5eea
rohanmangat <rohan.mangat@gmail.com>
2023-04-07 23:41:19 +0100

    clean data file

--- neuralnetwork4v2.py
+++ neuralnetwork4v2.py
@@ -1,5 +1,6 @@
-# best model to compare results for each individual dataset with a predicted model
-
+# same as neuralnetwork4
+# train with datasets 5 and 6, test on 7 only
+# IGNORE  - USE NEURALNETWORK4V3
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
@@ -11,58 +12,109 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 
 
-# import dataframes from main - CHOOSE DATASET BASED ON NUMBER AFTER FRAMES
+# open and append datasets 5 and 6 (train)
+pklfiles = ['frames.pkl', 'frames2.pkl']
+frames = []
+
+for filename in pklfiles:
+    # import dataframes from main
+    with open(filename, 'rb') as handle:
+        frames1 = pickle.load(handle)
+
+    frames.append(frames1)
+
+
+# open dataset 7 (test)
 with open('frames3.pkl', 'rb') as handle:
-    frames = pickle.load(handle)
+    frames_testdata = pickle.load(handle)
 
 # ------------------------- Sorting data input ------------------------- #
-df1 = frames[0]
-df2 = frames[1]
-df3 = frames[2]
+# for datasets 5 and 6 (train)
+df1 = frames[0][0]
+df2 = frames[0][1]
+df3 = frames[0][2]
+df4 = frames[1][0]
+df5 = frames[1][1]
+df6 = frames[1][2]
 
-allframes = [df1, df2, df3]
+frameset1 = [df1, df2, df3]
+frameset2 = [df4, df5, df6]
 
-# combining all dataframes
-dfcomb = pd.concat(allframes, axis=1)
-print(dfcomb.shape)
 
-dfcomb = dfcomb.drop(['SOH charge cycles', 'SOH discharge cycles', 'SOH discharge cycles 2', 'SOH discharge 2'], axis=1)
-print(dfcomb.shape)
+# for dataset 7 (test)
+df7 = frames_testdata[0]
+df8 = frames_testdata[1]
+df9 = frames_testdata[2]
+
+frameset3 = [df7, df8, df9]
+
+
+
+allframes = [frameset1, frameset2]
+dfcomb_final = pd.DataFrame()
+
+# combining dataframes for 5 and 6 only
+for frameset in allframes:
+    dfcomb = pd.concat(frameset, axis=1)
+    print(dfcomb.shape)
+
+    dfcomb = dfcomb.drop(['SOH charge cycles', 'SOH discharge cycles', 'SOH discharge cycles 2', 'SOH discharge 2'], axis=1)
+    print(dfcomb.shape)
+
+    # fixing issue of value stored as lists:
+    dfcomb = dfcomb.applymap(lambda x: x[0] if isinstance(x, list) and len(x) == 1 else x)
+
+    # find average SOH for charge and discharge, then remove those 2 columns
+    dfcomb['Average SOH'] = dfcomb[['SOH charge', 'SOH discharge']].mean(axis=1)
+    dfcomb = dfcomb.drop(['SOH charge', 'SOH discharge'], axis=1)
+
+    dfcomb_final = pd.concat([dfcomb, dfcomb_final], axis=0)
+
+
+
+# dataset 7 only:
+dfcomb2 = pd.concat(frameset3, axis=1)
+print(dfcomb2.shape)
+
+dfcomb2 = dfcomb2.drop(['SOH charge cycles', 'SOH discharge cycles', 'SOH discharge cycles 2', 'SOH discharge 2'], axis=1)
+print(dfcomb2.shape)
 
 # fixing issue of value stored as lists:
-dfcomb = dfcomb.applymap(lambda x: x[0] if isinstance(x, list) and len(x) == 1 else x)
+dfcomb2 = dfcomb2.applymap(lambda x: x[0] if isinstance(x, list) and len(x) == 1 else x)
 
 # find average SOH for charge and discharge, then remove those 2 columns
-dfcomb['Average SOH'] = dfcomb[['SOH charge', 'SOH discharge']].mean(axis=1)
-dfcomb = dfcomb.drop(['SOH charge', 'SOH discharge'], axis=1)
+dfcomb2['Average SOH'] = dfcomb2[['SOH charge', 'SOH discharge']].mean(axis=1)
+dfcomb2 = dfcomb2.drop(['SOH charge', 'SOH discharge'], axis=1)
+
+
+
 
 
 
 
 # remove NaNs from the whole dataset:
+print('Shape before:', dfcomb_final.shape)
 
-print('No. NaN values: ', dfcomb.isnull().sum().sum())
+print('No. NaN values: ', dfcomb_final.isnull().sum().sum())
 
-dfcomb = dfcomb.dropna(axis=0, how='any')
+dfcomb_final = dfcomb_final.dropna(axis=0, how='any')
 
-print('Shape after:', dfcomb.shape)
+print('Shape after:', dfcomb_final.shape)
 
-
-
+# saving dataframe to use to plot at end of script:
+with open('dfcomb_final.pkl', 'wb') as handle:
+    pickle.dump(dfcomb_final, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 
 
 # normalising data:
 scaler = MinMaxScaler()
 
-col_names = list(dfcomb.columns)
-row_num = list(dfcomb.index)
+col_names = list(dfcomb_final.columns)
+row_num = list(dfcomb_final.index)
 
-dfcomb_final_scaled = scaler.fit_transform(dfcomb.to_numpy())
+dfcomb_final_scaled = scaler.fit_transform(dfcomb_final.to_numpy())
 dfcomb_final = pd.DataFrame(dfcomb_final_scaled, columns=col_names, index=row_num)
-
-
-
 
 
 
@@ -115,6 +167,7 @@ model = MyModule(num_inputs=6, num_outputs=1, hidden_size=19)
 
 # criterion = torch.nn.MSELoss(size_average=False)
 loss_fn = torch.nn.MSELoss()
+
 # optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
 optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
 
@@ -130,8 +183,12 @@ y_test_tensor = torch.tensor(y_test.values, dtype=torch.float32).reshape(-1, 1)
 
 # training and test data
 train_dataloader = DataLoader(list(zip(X_train_tensor, y_train_tensor)), batch_size=32)
+# train_dataloader = DataLoader(list(zip(X_train_tensor, y_train_tensor)), batch_size=32, shuffle=True)
+
 
 test_dataloader = DataLoader(list(zip(X_test_tensor, y_test_tensor)), batch_size=32)
+# test_dataloader = DataLoader(list(zip(X_test_tensor, y_test_tensor)), batch_size=32, shuffle=True)
+
 
 # Check it's working
 for batch, (X, y) in enumerate(train_dataloader):
@@ -152,6 +209,7 @@ for epoch in range(num_epochs):
     batch_loss = []
     # training losses:
     for X, y in train_dataloader:
+        model.train()
         optimizer.zero_grad()
         pred = model(X)
         loss = loss_fn(pred, y)
@@ -169,6 +227,8 @@ for epoch in range(num_epochs):
         val_losses = []
         model.eval()
         for X, y in test_dataloader:
+            # model.eval()
+
             outputs = model(X)
             val_results.append(outputs.numpy())
             val_loss = loss_fn(outputs, y)
@@ -185,7 +245,6 @@ plt.plot(training_losses, label='Training Loss')
 plt.plot(validation_losses, label='Validation Loss')
 plt.xlabel('Epochs')
 plt.ylabel('Loss')
-plt.title('Battery B0007')
 plt.legend()
 
 
@@ -204,21 +263,23 @@ unnormalized_val_results = val_results * soh_range + soh_min
 
 print(X_test.index, unnormalized_val_results)
 plt.figure(2)
+# plt.plot(X_test.index, val_results, label='Predicted SOH')
 plt.scatter(X_test.index, unnormalized_val_results, label='Predicted SOH')
 plt.xlabel('Cycle Number')
 plt.ylabel('SOH')
 plt.legend()
 
 
-
 # plot true results on same graph:
-plt.scatter(dfcomb.index, dfcomb['Average SOH'], label='True SOH')
-plt.title('Battery B0007')
-plt.legend()
+with open('dfcomb_final.pkl', 'rb') as handle:
+    dfcomb_final = pickle.load(handle)
 
+
+plt.scatter(dfcomb_final.index, dfcomb_final['Average SOH'], label='True SOH')
+plt.legend()
 
 plt.show()
 
-
+# dfcomb = pd.concat(frameset, axis=1)
 
 print('hello')
--- neuralnetwork4v3.py
+++ neuralnetwork4v3.py
@@ -1,5 +1,6 @@
-# best model to compare results for each individual dataset with a predicted model
-
+# same as neuralnetwork4
+# train with datasets 5 and 6, test on 7 only
+# approach: same as neuralnetwork4 but remove last n rows of whole dataset which belong to dataset 7 and make that as X and Y for train_test split
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
@@ -10,73 +11,98 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 
+pklfiles = ['frames.pkl', 'frames2.pkl', 'frames3.pkl']
+frames = []
 
-# import dataframes from main - CHOOSE DATASET BASED ON NUMBER AFTER FRAMES
-with open('frames3.pkl', 'rb') as handle:
-    frames = pickle.load(handle)
+for filename in pklfiles:
+    # import dataframes from main
+    with open(filename, 'rb') as handle:
+        frames1 = pickle.load(handle)
+
+    frames.append(frames1)
 
 # ------------------------- Sorting data input ------------------------- #
-df1 = frames[0]
-df2 = frames[1]
-df3 = frames[2]
+df1 = frames[0][0]
+df2 = frames[0][1]
+df3 = frames[0][2]
+df4 = frames[1][0]
+df5 = frames[1][1]
+df6 = frames[1][2]
+df7 = frames[2][0]
+df8 = frames[2][1]
+df9 = frames[2][2]
 
-allframes = [df1, df2, df3]
+frameset1 = [df1, df2, df3]
+frameset2 = [df4, df5, df6]
+frameset3 = [df7, df8, df9]
+
+allframes = [frameset1, frameset2, frameset3]
+dfcomb_final = pd.DataFrame()
 
 # combining all dataframes
-dfcomb = pd.concat(allframes, axis=1)
-print(dfcomb.shape)
+for frameset in allframes:
+    dfcomb = pd.concat(frameset, axis=1)
+    print(dfcomb.shape)
 
-dfcomb = dfcomb.drop(['SOH charge cycles', 'SOH discharge cycles', 'SOH discharge cycles 2', 'SOH discharge 2'], axis=1)
-print(dfcomb.shape)
+    dfcomb = dfcomb.drop(['SOH charge cycles', 'SOH discharge cycles', 'SOH discharge cycles 2', 'SOH discharge 2'], axis=1)
+    print(dfcomb.shape)
 
-# fixing issue of value stored as lists:
-dfcomb = dfcomb.applymap(lambda x: x[0] if isinstance(x, list) and len(x) == 1 else x)
+    # fixing issue of value stored as lists:
+    dfcomb = dfcomb.applymap(lambda x: x[0] if isinstance(x, list) and len(x) == 1 else x)
 
-# find average SOH for charge and discharge, then remove those 2 columns
-dfcomb['Average SOH'] = dfcomb[['SOH charge', 'SOH discharge']].mean(axis=1)
-dfcomb = dfcomb.drop(['SOH charge', 'SOH discharge'], axis=1)
+    # find average SOH for charge and discharge, then remove those 2 columns
+    dfcomb['Average SOH'] = dfcomb[['SOH charge', 'SOH discharge']].mean(axis=1)
+    dfcomb = dfcomb.drop(['SOH charge', 'SOH discharge'], axis=1)
 
-
-
+    dfcomb_final = pd.concat([dfcomb, dfcomb_final], axis=0)
 
 # remove NaNs from the whole dataset:
+print('Shape before:', dfcomb_final.shape)
 
-print('No. NaN values: ', dfcomb.isnull().sum().sum())
+print('No. NaN values: ', dfcomb_final.isnull().sum().sum())
 
-dfcomb = dfcomb.dropna(axis=0, how='any')
+dfcomb_final = dfcomb_final.dropna(axis=0, how='any')
 
-print('Shape after:', dfcomb.shape)
+print('Shape after:', dfcomb_final.shape)
 
-
-
+# saving dataframe to use to plot at end of script:
+with open('dfcomb_final.pkl', 'wb') as handle:
+    pickle.dump(dfcomb_final, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 
 
 # normalising data:
 scaler = MinMaxScaler()
 
-col_names = list(dfcomb.columns)
-row_num = list(dfcomb.index)
+col_names = list(dfcomb_final.columns)
+row_num = list(dfcomb_final.index)
 
-dfcomb_final_scaled = scaler.fit_transform(dfcomb.to_numpy())
+dfcomb_final_scaled = scaler.fit_transform(dfcomb_final.to_numpy())
 dfcomb_final = pd.DataFrame(dfcomb_final_scaled, columns=col_names, index=row_num)
 
 
 
 
 
+#                                               get the x and y data:
+
+# get two different dataframes and randomise order of rows
+df_training = dfcomb_final[:-167]
+df_test = dfcomb_final.tail(167)
 
 
+df_training = df_training.sample(frac=1, random_state=22)
+df_test = df_test.sample(frac=1, random_state=22)
 
-# get the x and y data:
-X = dfcomb_final.drop('Average SOH', axis=1)
-print(X.shape)
 
-y = dfcomb_final['Average SOH']
-print(y.shape)
+# X and y data split for testing and training
 
-# Split data into x and y, with 30% for testing and randomly shuffling data
-(X_train, X_test, y_train, y_test) = train_test_split(X, y, test_size=0.3, random_state=22)
+X_train = df_training.drop('Average SOH', axis=1)
+y_train = df_training['Average SOH']
+
+X_test = df_test.drop('Average SOH', axis=1)
+y_test = df_test['Average SOH']
+
 
 print('yeee')
 
@@ -115,8 +141,9 @@ model = MyModule(num_inputs=6, num_outputs=1, hidden_size=19)
 
 # criterion = torch.nn.MSELoss(size_average=False)
 loss_fn = torch.nn.MSELoss()
-# optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
-optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
+
+optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+# optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
 
 
 # convert to pytorch tensors:
@@ -130,8 +157,12 @@ y_test_tensor = torch.tensor(y_test.values, dtype=torch.float32).reshape(-1, 1)
 
 # training and test data
 train_dataloader = DataLoader(list(zip(X_train_tensor, y_train_tensor)), batch_size=32)
+# train_dataloader = DataLoader(list(zip(X_train_tensor, y_train_tensor)), batch_size=32, shuffle=True)
+
 
 test_dataloader = DataLoader(list(zip(X_test_tensor, y_test_tensor)), batch_size=32)
+# test_dataloader = DataLoader(list(zip(X_test_tensor, y_test_tensor)), batch_size=32, shuffle=True)
+
 
 # Check it's working
 for batch, (X, y) in enumerate(train_dataloader):
@@ -152,6 +183,7 @@ for epoch in range(num_epochs):
     batch_loss = []
     # training losses:
     for X, y in train_dataloader:
+        model.train()
         optimizer.zero_grad()
         pred = model(X)
         loss = loss_fn(pred, y)
@@ -169,6 +201,8 @@ for epoch in range(num_epochs):
         val_losses = []
         model.eval()
         for X, y in test_dataloader:
+            # model.eval()
+
             outputs = model(X)
             val_results.append(outputs.numpy())
             val_loss = loss_fn(outputs, y)
@@ -185,7 +219,6 @@ plt.plot(training_losses, label='Training Loss')
 plt.plot(validation_losses, label='Validation Loss')
 plt.xlabel('Epochs')
 plt.ylabel('Loss')
-plt.title('Battery B0007')
 plt.legend()
 
 
@@ -204,21 +237,31 @@ unnormalized_val_results = val_results * soh_range + soh_min
 
 print(X_test.index, unnormalized_val_results)
 plt.figure(2)
+# plt.plot(X_test.index, val_results, label='Predicted SOH')
 plt.scatter(X_test.index, unnormalized_val_results, label='Predicted SOH')
 plt.xlabel('Cycle Number')
 plt.ylabel('SOH')
 plt.legend()
 
 
-
 # plot true results on same graph:
-plt.scatter(dfcomb.index, dfcomb['Average SOH'], label='True SOH')
-plt.title('Battery B0007')
+with open('dfcomb_final.pkl', 'rb') as handle:
+    dfcomb_final = pickle.load(handle)
+
+# # get results back in correct order
+# df_test = dfcomb_final.tail(167)
+#
+# # rewriting X_test and y_test to get original data before shuffling
+# X_test = df_test.drop('Average SOH', axis=1)
+# y_test = df_test['Average SOH']
+
+# .tail(167)
+
+# plt.scatter(dfcomb_final.index, dfcomb_final['Average SOH'], label='True SOH')
+plt.scatter(dfcomb_final.tail(167).index, dfcomb_final.tail(167)['Average SOH'], label='True SOH')
+
 plt.legend()
 
-
 plt.show()
-
-
 
 print('hello')
